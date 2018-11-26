3d2c96527bdb2cc98d1fcc912b26687b4e1f3578
Flynn84 <arkchib@gmail.com>
2018-11-26 18:13:14 +0300

    Mondays work!

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Jack Lemmon',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'November 23, 2018'
-    },
-    {
-        'author': 'Jim Jameson',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'November 24, 2018'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
